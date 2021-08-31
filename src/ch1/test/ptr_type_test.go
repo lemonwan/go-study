69ea3b9f65c8d30b522108a1ff0df4a4ce9c0891
lemonwan <1173816592@qq.com>
2021-08-31 23:17:19 +0800

    :new: 测试链表

--- src/ch1/test/ptr_type_test.go
+++ src/ch1/test/ptr_type_test.go
@@ -1,6 +1,9 @@
 package ptr_test
 
-import "testing"
+import (
+	"container/list"
+	"testing"
+)
 
 func TestType(t *testing.T) {
 	var ptr *int
@@ -8,4 +11,15 @@ func TestType(t *testing.T) {
 	ptr = &i
 	t.Log(ptr)
 	t.Log(*ptr)
+}
+
+func TestList(t *testing.T) {
+	li := list.New()
+	l1 := li.PushBack(4)
+	l2 := li.PushFront(1)
+	li.InsertAfter(2, l2)
+	li.InsertBefore(3, l1)
+	for e := li.Front(); e != nil; e = e.Next() {
+		t.Log(e.Value)
+	}
 }
