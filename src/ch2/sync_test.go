5afcd564febeb27b398f97a26017a8ddbbf5d7b9
lemonwan <1173816592@qq.com>
2021-09-27 23:18:13 +0800

    :new: add sync study

---
+++ src/ch2/sync_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestSync(t *testing.T) {
+	var mailbox uint8
+	var lock sync.RWMutex
+	sendCond := sync.NewCond(&lock)
+	recvCond := sync.NewCond(lock.RLocker())
+
+	sign := make(chan struct{}, 3)
+	max := 5
+
+	go func(max int) {
+		defer func() {
+			sign <- struct{}{}
+		}()
+		for i := 1; i <= 5; i++ {
+			time.Sleep(time.Millisecond * 500)
+			lock.Lock()
+			for mailbox == 1 {
+				sendCond.Wait()
+			}
+			t.Logf("sender [%d]: the mailbox is empty.", i)
+			mailbox = 1
+			t.Logf("sender [%d]: the letter has been sent.", i)
+			lock.Unlock()
+			recvCond.Signal()
+		}
+	}(max)
+
+	go func(max int) {
+		defer func() {
+			sign <- struct{}{}
+		}()
+		for i := 1; i <= max; i++ {
+			time.Sleep(time.Millisecond * 500)
+			lock.RLock()
+			for mailbox == 0 {
+				recvCond.Wait()
+			}
+			t.Logf("receiver [%d]: the mailbox is full.", i)
+			mailbox = 0
+			t.Logf("receiver [%d]: the letter has been received.", i)
+			lock.RUnlock()
+			sendCond.Signal()
+		}
+	}(max)
+
+	<-sign
+	<-sign
+}
