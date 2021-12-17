3fa69f099a1c707470459bae8e7bb8da959dac07
lemonwan <1173816592@qq.com>
2021-12-18 00:30:07 +0800

    新增多路选择代码学习

---
+++ ch2/channel_select_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func oneWork() chan string {
+	ch := make(chan string)
+	go func() {
+		fmt.Println("one work staring run")
+		time.Sleep(time.Second * 50)
+		ch <- "end"
+		fmt.Println("one work end")
+	}()
+	return ch
+}
+
+func twoWork() {
+	fmt.Println("two work staring run")
+	time.Sleep(time.Second * 10)
+	fmt.Println("two work end")
+}
+
+func TestSelectChannel(t *testing.T) {
+	twoWork()
+	select {
+	case ret := <-oneWork():
+		t.Log(ret)
+		// t.Log("all task run end")
+	case <-time.After(time.Millisecond * 30):
+		t.Error("timeout")
+	}
+}
