eba74d1c56b9cdb5f90061c80ad24feb4d77ae17
lemonwan <1173816592@qq.com>
2021-12-19 11:23:34 +0800

    :clapper: 新增通道关闭代码学习

---
+++ ch2/channel_close_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+// 生产者
+func producer(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		wg.Done()
+	}()
+}
+
+// 消费者
+// 一对一的生产和消费，可以保证程序正常运行
+// 如果是一生产对多个消费，则会出现程序卡主的问题，通道阻塞
+func consumer(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch)
+		}
+		wg.Done()
+	}()
+}
+
+func TestChannelClose(t *testing.T) {
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	producer(ch, &wg)
+	wg.Add(1)
+	consumer(ch, &wg)
+	wg.Wait()
+}
+
+// 改进程序
+func newProducter(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+		wg.Done()
+	}()
+}
+
+func newConsumer(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for {
+			if data, ok := <-ch; ok {
+				fmt.Println(data)
+			} else {
+				break
+			}
+		}
+		wg.Done()
+	}()
+}
+
+func TestChannelCloseNew(t *testing.T) {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	wg.Add(1)
+	newProducter(ch, &wg)
+	wg.Add(1)
+	newConsumer(ch, &wg)
+	wg.Add(1)
+	newConsumer(ch, &wg)
+	wg.Wait()
+}
