e06e36114ff37baba2cc691a2510dde6053c13e3
lemonwan <1173816592@qq.com>
2022-03-19 23:12:46 +0800

    :new: add study code

--- ch5/functional/web/web.go
+++ ch5/functional/web/web.go
@@ -9,11 +9,26 @@ import (
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
+type userError interface {
+	error
+	Message() string
+}
+
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Panic: %v", err)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
 			log.Printf("Error handling request: %s", err.Error())
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
