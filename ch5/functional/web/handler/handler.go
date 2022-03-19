a958c6746a09409c7d6344aaf6a27eddefe2a59b
lemonwan <1173816592@qq.com>
2022-03-19 22:34:33 +0800

    :new: add study code

---
+++ ch5/functional/web/handler/handler.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func List(writer http.ResponseWriter, request *http.Request) error {
+	path := request.URL.Path[len("/list/"):]
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	contents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	writer.Write(contents)
+	return nil
+}
---
+++ ch5/functional/web/web.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"ch5/functional/web/handler"
+	"log"
+	"net/http"
+	"os"
+)
+
+type appHandler func(writer http.ResponseWriter, request *http.Request) error
+
+func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			log.Printf("Error handling request: %s", err.Error())
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer, http.StatusText(code), code)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/list/", errWrapper(handler.List))
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
