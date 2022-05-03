3e6a2408947e8ff646076ea4222e729cdf39e9a0
lemonwan <1173816592@qq.com>
2022-05-03 10:17:12 +0800

    :new: add gin study code

--- ch5/http/gin_server.go
+++ ch5/http/gin_server.go
@@ -3,7 +3,11 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"math/rand"
+	"time"
 )
+
+const keyRequestId = "requestID"
 
 func main() {
 	engine := gin.Default()
@@ -14,14 +18,25 @@ func main() {
 	}
 
 	engine.Use(func(context *gin.Context) {
-		log.Info("incoming request", zap.String("path", context.Request.URL.Path))
+		s := time.Now()
+		context.Next()
+		log.Info("incoming request",
+			zap.String("path", context.Request.URL.Path),
+			zap.Int("status", context.Writer.Status()),
+			zap.Duration("time spent", time.Now().Sub(s)))
+	}, func(context *gin.Context) {
+		context.Set(keyRequestId, rand.Int())
 		context.Next()
 	})
 
 	engine.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
+		h := gin.H{
 			"message": "pong",
-		})
+		}
+		if requestId, exists := context.Get(keyRequestId); exists {
+			h[keyRequestId] = requestId
+		}
+		context.JSON(200, h)
 	})
 
 	engine.GET("/hello", func(context *gin.Context) {
