d68148135553370180cc88a45fd0b556d95accb7
lemonwan <1173816592@qq.com>
2022-05-14 20:06:24 +0800

    :new: 二叉树操作

--- ch5/testing/string_test.go
+++ ch5/testing/string_test.go
@@ -24,5 +24,42 @@ func TestEncode(t *testing.T) {
 		ch := fmt.Sprintf("%c", r)
 		t.Log(ch)
 	}
+}
 
+func TestArr(t *testing.T) {
+	var arr1 [3]int = [3]int{1, 2, 3}
+	arr2 := new([3]int)
+	t.Logf("%T", arr1)
+	t.Logf("%T", arr2)
+}
+
+type treeNode struct {
+	value       string
+	left, right *treeNode
+}
+
+func (node *treeNode) traverse() {
+	fmt.Println(node.value)
+	if node.left != nil {
+		node.left.traverse()
+	}
+	if node.right != nil {
+		node.right.traverse()
+	}
+}
+
+func createTreeNode() treeNode {
+	root := treeNode{"A", nil, nil}
+	root.left = &treeNode{value: "B"}
+	root.right = &treeNode{value: "C"}
+	root.left.left = &treeNode{value: "D"}
+	root.left.right = &treeNode{value: "E"}
+	root.right.left = &treeNode{value: "F"}
+	root.right.right = &treeNode{value: "G"}
+	return root
+}
+
+func TestPrintNode(t *testing.T) {
+	node := createTreeNode()
+	node.traverse()
 }
